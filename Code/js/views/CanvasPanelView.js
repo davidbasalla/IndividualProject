a9a7fbf480e616a1f812af32839810481b5c2930
davidbasalla <davidbasalla@gmail.com>
2014-06-01 16:04:40 +0100

    works for one view, not others tho...

--- Code/js/views/CanvasPanelView.js
+++ Code/js/views/CanvasPanelView.js
@@ -7,27 +7,149 @@ define(["text!templates/CanvasPanel.html"], function(CanvasPanelTemplate) {
 	},
 	initialize:function() {
 	    //possible vars, should these be in a model?
-
-	    this.title = "";
-	    this.container = "";
+	    
 	    //this.layer
 	    //this.view
-	    
+
+	    this.title = "";
+	    this.mode = "";
+	    this.container = "";
+
+	    //event listener for file loaded
+	    Backbone.on('fileLoaded', this.loadFile, this);
+	    Backbone.on('fileRemoved', this.clearFile, this);
+
+	    //create place holder for render data
+	    this.createData();
+
 	},
 	render:function() {
+
+	    switch (this.mode) {
+	    case "3D":
+		this.container = "viewer3D";
+		break;
+	    case "X":
+		this.container = "viewerX";
+		break;
+	    case "Y":
+		this.container = "viewerY";
+		break;
+	    case "Z":
+		this.container = "viewerZ";
+		break;
+	    }
+	    
 	    this.$el.html(this.template({title: this.title, container: this.container}));
-
-	    // create the 2D renderers
-
-	    /*
-	    this.sliceX = new X.renderer2D();
-	    this.sliceX.container = this.title;
-	    this.sliceX.orientation = 'X';
-	    this.sliceX.init();
-	    */
 
 	    return this; //to enable chain calling
 	},
+	initViewer:function(){
+	    
+	    // create the viewer
+	    if(this.mode == "3D")
+		this.viewer = new X.renderer3D();
+	    else
+		this.viewer = new X.renderer2D();
+	    
+	    this.viewer.container = this.container;
+
+	    if(this.mode == "X")
+		this.viewer.orientation = 'X';
+	    else if (this.mode == "Y")
+		this.viewer.orientation = 'Y';
+	    (this.mode == "Z")
+	    this.viewer.orientation = 'Z';
+
+	    this.viewer.init();
+	},
+	loadFile:function(file){
+
+	    console.log('XtkViewer.loadItem()');
+
+	    var f = file;
+	    var _fileName = f.name;
+	    var _fileExtension = _fileName.split('.').pop().toUpperCase();
+	    
+	    //add data to data_holder
+	    this._data['volume']['file'].push(f);
+
+	    //load files
+	    _this = this;
+	    
+	    this._types.forEach(function(v) {
+
+		if (_this._data[v]['file'].length > 0) {
+		    
+		    _this._data[v]['file'].forEach(function(u) {
+			
+			var reader = new FileReader();
+			
+			reader.onerror = _this.errorHandler;
+			reader.onload = (_this.loadHandler)(v,u); // bind the current type
+			
+			// start reading this file
+			reader.readAsArrayBuffer(u);
+		    });
+		}
+	    });
+	},
+	createData:function() {
+	    // the data holder for the scene
+	    // includes the file object, file data and valid extensions for each object
+	    
+	    this._data = {
+		'volume': {
+		    'file': [],
+		    'filedata': [],
+		    'extensions': ['NRRD']
+		},
+	    };
+
+	    //FROM SLICEDROP, MAKE SURE TO UNDERSTAND THIS
+	    this._types = Object.keys(this._data);
+	    // number of total files - legacy from SliceDrop code
+	    this._numberOfFiles = 1;
+	    this._numberRead = 0;
+	},
+	loadHandler:function(type,file){
+
+	    _this = this;
+	    
+	    return function(e) {
+		// reading complete
+
+		var data = e.target.result;
+		
+		// might have multiple files associated
+		// attach the filedata to the right one
+		
+		_this._data[type]['filedata'][_this._data[type]['file'].indexOf(file)] = data;
+		//console.log(_this._data[type]['filedata'][_this._data[type]['file'].indexOf(file)]);
+		
+		_this._numberRead++;
+		if (_this._numberRead == _this._numberOfFiles) {
+		    
+		    // all done, start the parsing
+		    //parse(_data);   
+		    
+		    // we have a volume
+		    _this.volume = new X.volume();
+		    //console.log('aboutToLoad volume.file');
+
+		    _this.volume.file = _this._data['volume']['file'].map(function(v) {
+			return v.name;
+		    });
+
+		    _this.volume.filedata = _this._data['volume']['filedata'];
+		    
+		    _this.viewer.add(_this.volume);
+		    _this.viewer.render();
+		}
+	    };
+	},
+
+	
     });
     return ViewerWindowView;
 });
--- Code/js/views/ViewerWindowView.js
+++ Code/js/views/ViewerWindowView.js
@@ -15,26 +15,29 @@ define(["views/CanvasPanelView"], function(CanvasPanel) {
 
 	    var viewer1 = new CanvasPanel();
 	    viewer1.title = 'viewer1';
-	    viewer1.container = "slice3D";
+	    viewer1.mode = "3D";
 
 	    var viewer2 = new CanvasPanel();
 	    viewer2.title = 'viewer2';
-	    viewer2.container = "sliceX";
+	    viewer2.mode = "X";
 
 	    var viewer3 = new CanvasPanel();
 	    viewer3.title = 'viewer3';
-	    viewer3.container = "sliceY";
+	    viewer3.mode = "Y";
 
 	    var viewer4 = new CanvasPanel();
 	    viewer4.title = 'viewer4';
-	    viewer4.container = "sliceZ";
+	    viewer4.mode = "Z";
 
-	    
 	    this.$el.append(viewer1.render().el);
 	    this.$el.append(viewer2.render().el);
 	    this.$el.append(viewer3.render().el);
 	    this.$el.append(viewer4.render().el);
-	    
+
+	    viewer1.initViewer();
+	    viewer2.initViewer();
+	    viewer3.initViewer();
+	    viewer4.initViewer();
 	},
     });
     return ViewerWindowView;
